2ec0f62082ff90e215c6d26ae114aaf9c8774c21
Bas Grolleman <bgrolleman@emendo-it.nl>
2011-12-11 23:52:26 +0100

    Work on footer, single line, no customizing, grey

--- footer.php
+++ footer.php
@@ -5,16 +5,15 @@
 <!-- end page -->
 <div id="footer-bgcontent">
 <div id="footer">
-	<p class="legal">&copy;2008
+<p class="legal">&copy;2011
 		<?php bloginfo('name'); ?>
-		&nbsp;&nbsp;&bull;&nbsp;&nbsp; <a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a> &nbsp;&nbsp;&bull;&nbsp;&nbsp; <a href="<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a> &nbsp;&nbsp;&bull;&nbsp;&nbsp;
-		
-		Powered by <a href="http://wordpress.org/">WordPress</a>.
 		&nbsp;&nbsp;&bull;&nbsp;&nbsp;
-		Designed by <a href="http://www.freewpthemes.net/">Free WordPress Themes</a>. </p>
-		
-		<div class="menu"><?php wp_nav_menu ( array('theme_location' => 'footer')); ?></div>
-
+		Ontwerp door <a href="http://www.freewpthemes.net/">Free WordPress Themes</a>
+		&nbsp;&nbsp;&bull;&nbsp;&nbsp;
+    Fotografie door <a href="http://www.maaikeappels.nl/">Maaike Appels Fotografie</a> 
+		&nbsp;&nbsp;&bull;&nbsp;&nbsp;
+    Hosting door <a href="http://www.emendo-it.nl/">eMendo-IT</a>
+</p>
 </div>
 </div>
 <?php wp_footer(); ?>
